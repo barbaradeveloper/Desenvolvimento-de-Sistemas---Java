086c9bd0d4826fd3dc3c8377b82c3221a4c8adef
barbaradeveloper <barbaramartins1245@gmail.com>
2021-03-16 21:11:24 -0300

    Atividade aula 12 prática - reaproveitamento de código

---
+++ Atividade12_BarbaradeSousa/src/Atividade12/Veiculo.java
@@ -0,0 +1,18 @@
+package Atividade12;
+
+public class Veiculo {
+	private String nome;
+	private String modelo;
+	private String marca;
+	private String ano;
+	private String cor;
+	
+	public class Carro extends Veiculo{
+		
+	}
+	public class Moto extends Veiculo{
+		
+	}
+	
+	
+}
