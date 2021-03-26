758e75176fa4e410d2b9d7fbe2beff81e9cb9da0
barbaradeveloper <barbaramartins1245@gmail.com>
2021-03-26 19:08:48 -0300

    Aula 13 Prática 02 Poo Java Reescrita de Método

---
+++ ativ_fix_heranca_02_BarbaradeSousa/src/atividadefixacao/Veiculo.java
@@ -0,0 +1,24 @@
+package atividadefixacao;
+
+
+public interface Veiculo {
+	
+	public int getidveiculo();
+	public void setidveiculo(int idveiculo);
+	
+	public String getnome();
+	public void setnome(String nome);
+	
+	public String getmodelo();
+	public void setmodelo(String modelo);
+	
+	public String getmarca();
+	public void setmarca(String marca);
+	
+	public String getano();
+	public void setano(String ano);
+	
+	public String getcor();
+	public void setcor(String cor);
+	
+}
