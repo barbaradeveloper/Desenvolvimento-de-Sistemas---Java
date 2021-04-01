92381a216511eab37b1f13900897bf171b7a9ca5
barbaradeveloper <barbaramartins1245@gmail.com>
2021-04-01 11:43:46 -0300

    Aula 22 Ds Escopo

---
+++ ativ_fix_escopo_BárbaradeSousa/src/escopoVariaveis/EscopoVariavel.java
@@ -0,0 +1,68 @@
+package escopoVariaveis;
+
+public class EscopoVariavel {
+
+	public static void main(String[] args) {
+		EscopoVariaveis escopo = new EscopoVariaveis();
+		escopo.setValor(10);
+		
+		System.out.println(escopo.getValor());
+		System.out.println(escopo.calculaValor(20));
+		System.out.println(escopo.getValor());
+		
+	}
+	private int valor;
+
+	public int getValor() {
+		return valor;
+	}
+
+	public void setValor(int valor) {
+		this.valor = valor;
+	}
+	public int calculaValor(int valor) {
+		valor = valor + 10;
+		return valor;
+	}
+		
+	
+	public int Teste() {
+		int valor = 5;
+		if(true) {
+			valor--;			
+		}
+		this.valor = valor;
+		return valor;
+	}
+	
+	public void outroTeste() {
+		for (int i = 0; i < 10; i++) {
+				System.out.println(i);
+			}
+	}
+		public void maisUmTeste() {
+			
+			boolean flag = true;
+			if(flag) {
+				int umaVariavel = 10;
+				umaVariavel++;
+				System.out.println(umaVariavel);
+				
+			}
+		}
+		public void maisUmOutroTeste(int num) {
+			
+		}
+		
+		// erro de compilação
+		
+		/* int j;
+		for(j = 0; j< 10; j ++) {
+			System.out.println(j);
+		}
+		}
+		*/
+	
+	
+	
+}
